cea131f7f0b3edd49713448fe5edd33127ea036b
Gary Richards <gary.i.richards@gmail.com>
2014-07-26 12:19:56 +0100

    Commit of rprog-005 Assignment 2. "Caching the Inverse of a Matrix"
    Write the following functions:
    
        makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
        cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
+## Provides functions
+##
+## get : returns the current matrix
+## set : sets a new matrix, and resets the cached inverse
+## getinverse : returns the solve function used to calculate the inverse
+## setinverse : sets a new solve function to calculate the inverse of the matrix
+##
 makeCacheMatrix <- function(x = matrix()) {
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) s <<- solve
+    getinverse <- function() s
 
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+## Write a short comment describing this function
+## Computes the inverse of a matrix that hasbeen created via 'makeCacheMatrix'
+## Returns the cached result of the solve if it has already been calculate, 
+## otherwise it calculates, caches and then returns the result of the solve
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$getinverse()
+    if (!is.null(s)) {
+        message("getting cached solve")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setinverse(s)
+    s
 }
